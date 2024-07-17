4e5335da50b1c0107bee4da839b5e3a87a5e7ecd
Divyanshu Tiwari <128217067+DivyanshuTiwari-1@users.noreply.github.com>
2024-07-17 16:57:25 +0530

    problem 2

--- Array.cpp
+++ Array.cpp
@@ -22,3 +22,31 @@ int findDuplicates(vector <int>& nums){
      }
      return left;
 }
+
+
+// sort color -------------------------------------
+class Solution {
+public:
+    
+   
+    void sortColors(vector<int>& nums) {
+       unordered_map<int,int>seen;
+       for (int i=0;i<nums.size();i++){
+        if (nums[i]==0||nums[i]==1|| nums[i]==2){
+            seen[nums[i]]++;
+        }
+       }
+       int j=0;
+       for (int i=0;i<3;i++){
+         while (seen[i]>0){
+         nums[j]=i;
+         j++;
+         seen[i]--;
+
+       }
+       }
+      
+    }
+};
+
+// kuch nhi ka karna bus 0,1,2 k count store kar lo map ke under phir nums ko replace kardo sorted way me bus
